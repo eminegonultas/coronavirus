2f6a93b77bbd74dbc8d281ea07c131ec80a74757
Sercan Yetkin <sercan@kaizentech.net>
2020-12-12 17:41:44 +0300

    loader added

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,6 +3,7 @@ import cm from "./home.module.css";
 import { getCountries } from "../../Services/service";
 import { VectorMap } from "react-jvectormap";
 import { useHistory } from "react-router-dom";
+import { SpinnerDotted } from "spinners-react";
 
 const Home = () => {
   let history = useHistory();
@@ -11,111 +12,125 @@ const Home = () => {
   const [recovered, setRecovered] = useState({});
   const [mapColorRange, setMapColorRange] = useState(["#ffffff", "#000000"]);
   const [selectedButton, setSelectedButton] = useState({});
+  const [loader, setLoader] = useState(true);
   //deneme
   useEffect(() => {
-    // getCountries()
-    //   .then(
-    //     (res) => {
-    //       // eslint-disable-next-line
-    //       res.data.map((e, i) => {
-    //         if (e.casesPerOneMillion > 0) {
-    //           setDeaths((state) => ({
-    //             ...state,
-    //             [e.countryInfo.iso2]: e.deathsPerOneMillion,
-    //           }));
-    //           setRecovered((state) => ({
-    //             ...state,
-    //             [e.countryInfo.iso2]: e.recoveredPerOneMillion,
-    //           }));
-    //           setTotalCases((state) => ({
-    //             ...state,
-    //             [e.countryInfo.iso2]: e.casesPerOneMillion,
-    //           }));
-    //         }
-    //       });
-    //     },
-    //     () => {
-    //       console.log("end");
-    //     }
-    //   )
-    //   .catch((err) => console.log(err));
+    getCountries()
+      .then(
+        (res) => {
+          // eslint-disable-next-line
+          let wait = res.data.map((e, i) => {
+            if (e.casesPerOneMillion > 0) {
+              setDeaths((state) => ({
+                ...state,
+                [e.countryInfo.iso2]: e.deathsPerOneMillion,
+              }));
+              setRecovered((state) => ({
+                ...state,
+                [e.countryInfo.iso2]: e.recoveredPerOneMillion,
+              }));
+              setTotalCases((state) => ({
+                ...state,
+                [e.countryInfo.iso2]: e.casesPerOneMillion,
+              }));
+            }
+            return "";
+          });
+          Promise.all(wait).then(() => {
+            setLoader(false);
+          });
+        },
+        () => {}
+      )
+      .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div className={cm.body}>
-      <button
-        className={cm.button}
-        onClick={() => {
-          setMapColorRange(["#ff9999", "#b30000"]);
-          setSelectedButton(deaths);
-        }}
-      >
-        Deaths
-      </button>
-      <button
-        className={cm.button}
-        onClick={() => {
-          setMapColorRange(["#fefefe", "#f30000"]);
-          setSelectedButton(totalCases);
-        }}
-      >
-        Total Cases
-      </button>
-      <button
-        className={cm.button}
-        onClick={() => {
-          setMapColorRange(["#000000", "#ffffff"]);
-          setSelectedButton(recovered);
-        }}
-      >
-        Recovered
-      </button>
-      <button
-        className={cm.button}
-        onClick={() => {
-          history.push("/compare");
-        }}
-      >
-        Compare
-      </button>
-      <VectorMap
-        map={"world_mill"}
-        backgroundColor="transparent" //change it to ocean blue: #0077be
-        zoomOnScroll={true}
-        containerStyle={{
-          width: "100%",
-          height: "300px",
-        }}
-        containerClassName="map"
-        regionStyle={{
-          initial: {
-            fill: "#e4e4e4",
-            "fill-opacity": 0.9,
-            stroke: "none",
-            "stroke-width": 0,
-            "stroke-opacity": 0,
-          },
-          hover: {
-            "fill-opacity": 0.8,
-            cursor: "pointer",
-          },
-          selected: {
-            //fill: "#2938bc", //color for the clicked country
-          },
-          selectedHover: {},
-        }}
-        regionsSelectable={false}
-        series={{
-          regions: [
-            {
-              values: selectedButton, //this is your data
-              scale: mapColorRange, //your color game's here
-              normalizeFunction: "linear",
-            },
-          ],
-        }}
-      />
-    </div>
+    <>
+      <div className={cm.body}>
+        {loader ? (
+          <div className={cm.spinner}>
+            <SpinnerDotted enabled={true} />
+          </div>
+        ) : (
+          <>
+            {" "}
+            <button
+              className={cm.button}
+              onClick={() => {
+                setMapColorRange(["#ff9999", "#b30000"]);
+                setSelectedButton(deaths);
+              }}
+            >
+              Deaths
+            </button>
+            <button
+              className={cm.button}
+              onClick={() => {
+                setMapColorRange(["#fefefe", "#f30000"]);
+                setSelectedButton(totalCases);
+              }}
+            >
+              Total Cases
+            </button>
+            <button
+              className={cm.button}
+              onClick={() => {
+                setMapColorRange(["#000000", "#ffffff"]);
+                setSelectedButton(recovered);
+              }}
+            >
+              Recovered
+            </button>
+            <button
+              className={cm.button}
+              onClick={() => {
+                history.push("/compare");
+              }}
+            >
+              Compare
+            </button>
+            <VectorMap
+              map={"world_mill"}
+              backgroundColor="transparent" //change it to ocean blue: #0077be
+              zoomOnScroll={true}
+              containerStyle={{
+                width: "100%",
+                height: "300px",
+              }}
+              containerClassName="map"
+              regionStyle={{
+                initial: {
+                  fill: "#e4e4e4",
+                  "fill-opacity": 0.9,
+                  stroke: "none",
+                  "stroke-width": 0,
+                  "stroke-opacity": 0,
+                },
+                hover: {
+                  "fill-opacity": 0.8,
+                  cursor: "pointer",
+                },
+                selected: {
+                  //fill: "#2938bc", //color for the clicked country
+                },
+                selectedHover: {},
+              }}
+              regionsSelectable={false}
+              series={{
+                regions: [
+                  {
+                    values: selectedButton, //this is your data
+                    scale: mapColorRange, //your color game's here
+                    normalizeFunction: "linear",
+                  },
+                ],
+              }}
+            />
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
